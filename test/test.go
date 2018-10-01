76a74cb4c55c2dfaab0e8cfa4c6c43fb66155970
Michael Driscoll <fenris@ulfheim.net>
2018-10-01 09:17:34 -0600

    Unused function

--- test/test.go
+++ test/test.go
@@ -12,15 +12,6 @@ import (
 
 var CallerDepth = 1
 
-// assert fails the test if the condition is false.
-func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
-	if !condition {
-		_, file, line, _ := runtime.Caller(CallerDepth)
-		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
-	}
-}
-
 // ok fails the test if an err is not nil.
 func Ok(tb testing.TB, err error) {
 	if err != nil {
